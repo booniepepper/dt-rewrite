1a207a053d67119b3ad13dc312cee6781320c6a9
booniepepper <justin@so.dang.cool>
2023-10-30 17:07:39 -0700

    use builtin fns and less of std.meta.trait

--- src/mem.zig
+++ src/mem.zig
@@ -2,28 +2,32 @@ const std = @import("std");
 
 pub const canClone = std.meta.trait.hasFn("clone");
 
-const canRelease = std.meta.trait.hasFn("release");
-const canDeinit = std.meta.trait.hasFn("deinit");
-const hasItems = std.meta.trait.hasField("items");
-const hasKeys = std.meta.trait.hasFn("keyIterator");
-const hasValues = std.meta.trait.hasFn("valueIterator");
-
 pub fn free(something: anytype) void {
     const T = @TypeOf(something);
     var thing = something;
 
+    // Pointers
     if (comptime std.meta.trait.isSingleItemPtr(T)) return free(thing.*);
 
-    if (comptime hasItems(T)) for (thing.items) |item| free(item);
-    if (comptime hasKeys(T)) {
+    // "Primitives"
+    if (!comptime std.meta.trait.isContainer(T)) return;
+
+    // ArrayList
+    if (comptime @hasField(T, "items")) for (thing.items) |item| free(item);
+
+    // HashMap
+    if (comptime @hasDecl(T, "keyIterator")) {
         var it = thing.keyIterator();
         while (it.next()) |key| free(key);
     }
-    if (comptime hasValues(T)) {
+    if (comptime @hasDecl(T, "valueIterator")) {
         var it = thing.valueIterator();
         while (it.next()) |value| free(value);
     }
 
-    if (comptime canDeinit(T)) thing.deinit();
-    if (comptime canRelease(T)) thing.release();
+    // Many things
+    if (comptime @hasDecl(T, "deinit")) thing.deinit();
+
+    // Rc
+    if (comptime @hasDecl(T, "release")) thing.release();
 }
