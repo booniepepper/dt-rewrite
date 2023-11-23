020f70c3f7b5b16c33cb79dd38e01cb00dbe37b6
booniepepper <justin@so.dang.cool>
2023-11-22 16:34:38 -0800

    move Rc to mem.zig

--- src/mem.zig
+++ src/mem.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const Dictionary = @import("types.zig").Dictionary;
 
 pub const canClone = std.meta.trait.hasFn("clone");
 
@@ -30,4 +33,76 @@ pub fn free(something: anytype) void {
 
     // Rc
     if (comptime @hasDecl(T, "release")) thing.release();
+}
+
+/// A reference-counted thing.
+pub fn Rc(comptime IT: type) type {
+    return struct {
+        allocator: Allocator,
+        it: IT,
+        refs: *usize,
+
+        const Self = @This();
+
+        pub fn new(allocator: Allocator) !Self {
+            const it = if (comptime std.meta.trait.hasFn("init")(IT))
+                IT.init(allocator)
+            else if (comptime std.meta.trait.hasFn("new")(IT))
+                try IT.new(allocator)
+            else
+                undefined;
+            const refs = try allocator.create(usize);
+            refs.* = 1;
+            return .{
+                .allocator = allocator,
+                .it = it,
+                .refs = refs,
+            };
+        }
+
+        /// Creates a new reference.
+        pub fn newref(self: Self) Self {
+            self.refs.* += 1;
+            return .{
+                .allocator = self.allocator,
+                .it = self.it,
+                .refs = self.refs,
+            };
+        }
+
+        /// Creates a clone, allocating a new copy of "it."
+        pub fn clone(self: *Self) !Self {
+            var theClone = try Self.new(self.allocator);
+
+            if (IT == Dictionary) {
+                var entries = self.it.iterator();
+                while (entries.next()) |entry| {
+                    try theClone.it.put(entry.key_ptr.newref(), entry.value_ptr.newref());
+                }
+                return theClone;
+            }
+
+            try theClone.it.ensureTotalCapacity(self.it.items);
+
+            if (canClone(IT)) {
+                for (self.it.items) |item| {
+                    try theClone.it.append(item.clone());
+                }
+            } else {
+                try theClone.it.appendSlice(self.it.items);
+            }
+
+            return theClone;
+        }
+
+        /// Releases this reference. If this was the final reference, the
+        /// it and the counter are freed.
+        pub fn release(self: Self) void {
+            self.refs.* -= 1;
+            if (self.refs.* == 0) {
+                free(self.it);
+                self.allocator.destroy(self.refs);
+            }
+        }
+    };
 }
--- src/types.zig
+++ src/types.zig
@@ -7,6 +7,7 @@ const HashMap = std.HashMap;
 const stderr = std.io.getStdErr().writer();
 
 const mem = @import("mem.zig");
+const Rc = mem.Rc;
 const free = mem.free;
 const canClone = mem.canClone;
 
@@ -239,76 +240,4 @@ pub fn makeString(raw: []const u8, allocator: Allocator) !String {
     var string: String = try String.new(allocator);
     try string.it.appendSlice(raw);
     return string;
-}
-
-/// A reference-counted thing.
-pub fn Rc(comptime IT: type) type {
-    return struct {
-        allocator: Allocator,
-        it: IT,
-        refs: *usize,
-
-        const Self = @This();
-
-        pub fn new(allocator: Allocator) !Self {
-            const it = if (comptime std.meta.trait.hasFn("init")(IT))
-                IT.init(allocator)
-            else if (comptime std.meta.trait.hasFn("new")(IT))
-                try IT.new(allocator)
-            else
-                undefined;
-            const refs = try allocator.create(usize);
-            refs.* = 1;
-            return .{
-                .allocator = allocator,
-                .it = it,
-                .refs = refs,
-            };
-        }
-
-        /// Creates a new reference.
-        pub fn newref(self: Self) Self {
-            self.refs.* += 1;
-            return .{
-                .allocator = self.allocator,
-                .it = self.it,
-                .refs = self.refs,
-            };
-        }
-
-        /// Creates a clone, allocating a new copy of "it."
-        pub fn clone(self: *Self) !Self {
-            var theClone = try Self.new(self.allocator);
-
-            if (IT == Dictionary) {
-                var entries = self.it.iterator();
-                while (entries.next()) |entry| {
-                    try theClone.it.put(entry.key_ptr.newref(), entry.value_ptr.newref());
-                }
-                return theClone;
-            }
-
-            try theClone.it.ensureTotalCapacity(self.it.items);
-
-            if (canClone(IT)) {
-                for (self.it.items) |item| {
-                    try theClone.it.append(item.clone());
-                }
-            } else {
-                try theClone.it.appendSlice(self.it.items);
-            }
-
-            return theClone;
-        }
-
-        /// Releases this reference. If this was the final reference, the
-        /// it and the counter are freed.
-        pub fn release(self: Self) void {
-            self.refs.* -= 1;
-            if (self.refs.* == 0) {
-                free(self.it);
-                self.allocator.destroy(self.refs);
-            }
-        }
-    };
 }
